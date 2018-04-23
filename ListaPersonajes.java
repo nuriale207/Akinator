1be3c32aa6ef346ba41b338ff8bfd56948a5b102
sgainhoa <38654493+sgainhoa@users.noreply.github.com>
2018-04-23 12:50:28 +0200

    Update ListaPersonajes.java

--- ListaPersonajes.java
+++ ListaPersonajes.java
@@ -1,3 +1,10 @@
 public class ListaPersonajes{
-
+  
+  //atributos
+  private static ListaPersonajes miListaPersonajes=new ListaPersonajes();
+  private ArrayList<Personaje> lista;
+  private ListaAtributos listaTodosLosAtributos;
+  
+  //constructora
+  
 }

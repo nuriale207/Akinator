b2c39e63d232fa9a2e0fb75e6e7ae445daf1154b
nuriale207 <38654473+nuriale207@users.noreply.github.com>
2018-04-23 13:01:03 +0200

    Add files via upload

---
+++ jugarPartida.java
@@ -0,0 +1,14 @@
+package Akinator;
+
+public class jugarPartida {
+	
+	public jugarPartida() {
+		
+	}
+
+	public void jugarPartida() {
+		ListaPreguntas lista= null;
+		lista=lista.getListaPreguntas();
+		lista.realizarPregunta();
+	}
+}

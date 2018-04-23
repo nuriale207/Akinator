15fdc75fbaed3159e6df139eb088ead92d9e0bc2
nuriale207 <38654473+nuriale207@users.noreply.github.com>
2018-04-23 12:39:03 +0200

    Update ListaPreguntas.java

--- ListaPreguntas.java
+++ ListaPreguntas.java
@@ -1,3 +1,7 @@
 public class ListaPreguntas{
-  private a
+  private ArrayList<Pregunta> lista;
+  private static ListaPreguntas miListaPreguntas= new ListaPreguntas();
+  private ListaPreguntas(){
+    this. lista =new ArrayList<Pregunta>;
+  }
 }

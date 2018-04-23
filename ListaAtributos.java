1e2ca026ea2030763de17de8153f44633dfe9e33
sgainhoa <38654493+sgainhoa@users.noreply.github.com>
2018-04-23 13:17:57 +0200

    Update ListaAtributos.java

--- ListaAtributos.java
+++ ListaAtributos.java
@@ -1,6 +1,7 @@
 package org.pmoo.Akinator;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class ListaAtributos {
 	
@@ -11,5 +12,31 @@ public class ListaAtributos {
 	public ListaAtributos()
 	{
 		this.lista=new ArrayList<Atributo>();
+	}
+	
+	//getters
+	private Iterator<Atributo> getIterador()
+	{
+		return this.lista.iterator();
+	}
+	
+	//otros metodos
+	public boolean comprobarAtributo(String pAtributoRelacion)
+	{
+		boolean esta=false;
+		Iterator<Atributo>itr=getIterador();
+		Atributo unAtributo=null;
+		
+		while(itr.hasNext() && !esta)
+		{
+			unAtributo=itr.next();
+			if (unAtributo.getValor()==pAtributoRelacion)
+			{
+				esta=true;
+			}
+		}
+		
+		return esta;
+		
 	}
 }

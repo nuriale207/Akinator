1239aa74d98876076723a9c6ee219e34db304c58
sgainhoa <38654493+sgainhoa@users.noreply.github.com>
2018-04-23 12:51:08 +0200

    Add files via upload

---
+++ ListaAtributos.java
@@ -0,0 +1,15 @@
+package org.pmoo.Akinator;
+
+import java.util.ArrayList;
+
+public class ListaAtributos {
+	
+	//atributos
+	private ArrayList<Atributo>lista;
+	
+	//constructora
+	public ListaAtributos()
+	{
+		this.lista=new ArrayList<Atributo>();
+	}
+}

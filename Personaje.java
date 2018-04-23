5459001cf69f5c486a4f6d85cfc485a2f4ff2517
sgainhoa <38654493+sgainhoa@users.noreply.github.com>
2018-04-23 12:52:22 +0200

    Add files via upload

---
+++ Personaje.java
@@ -0,0 +1,31 @@
+package org.pmoo.Akinator;
+
+public class Personaje {
+	//atributos
+	private String nombre;
+	private ListaAtributos lista;
+	
+	//constructora
+	public Personaje (String pNombre, ListaAtributos pLista)
+	{
+		this.nombre=pNombre;
+		this.lista=pLista;
+	}
+	
+	//getters 
+	public String getNombre()
+	{
+		return this.nombre;
+	}
+	
+	public ListaAtributos getListaAtributos()
+	{
+		return this.lista;
+	}
+	
+	//otros metodos
+	public boolean comprobarAtributo(String pAtributoRelacion)
+	{
+		this.lista.comprobarAtributo();
+	}
+}

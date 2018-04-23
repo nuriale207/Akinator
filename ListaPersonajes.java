f343157dd9e19c55f95261ac40d54fc9b2a223df
sgainhoa <38654493+sgainhoa@users.noreply.github.com>
2018-04-23 13:09:57 +0200

    Update ListaPersonajes.java

--- ListaPersonajes.java
+++ ListaPersonajes.java
@@ -1,10 +1,25 @@
-public class ListaPersonajes{
-  
-  //atributos
-  private static ListaPersonajes miListaPersonajes=new ListaPersonajes();
-  private ArrayList<Personaje> lista;
-  private ListaAtributos listaTodosLosAtributos;
-  
-  //constructora
-  
+import java.util.ArrayList;
+
+public class ListaPersonajes {
+
+	//atributos
+	private static ListaPersonajes miListaPersonajes=new ListaPersonajes();
+	private ArrayList<Personaje> lista;
+	private ListaAtributos listaTodosLosAtributos;
+	  
+	//constructora
+	private ListaPersonajes ()
+	{
+		  this.lista=new ArrayList<Personaje>();
+		  this.listaTodosLosAtributos=new ListaAtributos();
+	}
+	
+	//getters
+	public ListaPersonajes getListaPersonajes()
+	{
+		return miListaPersonajes;
+	}
+	
+	//otros metodos
+	
 }

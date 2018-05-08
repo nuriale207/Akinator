ec20b99a076161f4bea393aa2cf3d12d45461243
sgainhoa <38654493+sgainhoa@users.noreply.github.com>
2018-05-08 16:11:33 +0200

    Update ListaPersonajesTest.java

--- ListaPersonajesTest.java
+++ ListaPersonajesTest.java
@@ -7,19 +7,42 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class ListaPersonajesTest {
-	Atributo a1,a2,a3;
+	Atributo a1,a2,a3,i1,i2,i3,i4,s1,s2,s3,a4;
 	ListaPersonajes l1;
-	Personaje p1,p2,p3;
+	Personaje p1,p2,p3,p4,p5;
 	@Before
 	public void setUp() throws Exception {
+		
 		p1=new Personaje("minion");
-		p2= new Personaje("bambi");
+		p2=new Personaje("bambi");
+		p3=new Personaje("Iron Man");
+		p4=new Personaje("spiderman");
+		p5=new Personaje("flash");
+		
 		a1=new Atributo("moreno");
 		a2=new Atributo("animal");
 		a3=new Atributo("alto");
+		i1=new Atributo("marvel");
+		i2=new Atributo("hombre");
+		i3=new Atributo("rico");
+		s3=new Atributo("humanoMutado");	
+		a4=new Atributo("dc");	
+		
 		p1.anadirAtributo(a1);
-		l1=ListaPersonajes.getListaPersonajes();
-		l1.anadirPersonaje(p1);
+		p2.anadirAtributo(a2);
+		
+		p3.anadirAtributo(i1);
+		p3.anadirAtributo(i2);
+		p3.anadirAtributo(i3);
+		
+		p4.anadirAtributo(i1);
+		p4.anadirAtributo(i2);
+		p4.anadirAtributo(s3);
+		
+		p5.anadirAtributo(i2);
+		p5.anadirAtributo(a4);
+		
+		ListaPersonajes.getListaPersonajes().anadirPersonaje(p1);
 	}
 
 	@After
@@ -35,31 +58,47 @@ public class ListaPersonajesTest {
 
 	@Test
 	public void testGetSize() {
-		l1=ListaPersonajes.getListaPersonajes();
-		assertEquals(l1.getSize(),1);
-		l1.anadirPersonaje(p2);
-		assertEquals(l1.getSize(),2);
+		assertEquals(ListaPersonajes.getListaPersonajes().getSize(),1);
+		ListaPersonajes.getListaPersonajes().anadirPersonaje(p2);
+		assertEquals(ListaPersonajes.getListaPersonajes().getSize(),2);
 	}
 
 	@Test
 	public void testAnadirPersonaje() {
-		l1.vaciarLista();
-		l1.anadirPersonaje(p1);
-		assertNotNull(l1);
-		assertEquals(l1.getSize(),1);
+		ListaPersonajes.getListaPersonajes().vaciarLista();
+		ListaPersonajes.getListaPersonajes().anadirPersonaje(p1);
+		assertNotNull(ListaPersonajes.getListaPersonajes());
+		assertEquals(ListaPersonajes.getListaPersonajes().getSize(),1);
+		ListaPersonajes.getListaPersonajes().anadirPersonaje(p2);
+		assertEquals(ListaPersonajes.getListaPersonajes().getSize(),2);
 	}
 	
 	@Test
 	public void testEliminarPersonajePorAtributo() {
-		//si se vac�a la lista entera da error
-		l1.anadirPersonaje(p2);
-		l1.eliminarPersonajePorAtributo(a1, false);
-		assertEquals(l1.getSize(),1);
+		//si se vacia la lista entera da error
+		
+		ListaPersonajes.getListaPersonajes().anadirPersonaje(p3);
+		ListaPersonajes.getListaPersonajes().anadirPersonaje(p2);
+		ListaPersonajes.getListaPersonajes().anadirPersonaje(p4);
+		
+		ListaPersonajes.getListaPersonajes().eliminarPersonajePorAtributo(i1, false);
+		assertEquals(ListaPersonajes.getListaPersonajes().getSize(),2);
 	}
 
 	@Test
 	public void testBuscarAtributoMasFrecuente() {
-
-		}
+		
+		ListaPersonajes.getListaPersonajes().anadirPersonaje(p3);
+		ListaPersonajes.getListaPersonajes().anadirPersonaje(p2);
+		ListaPersonajes.getListaPersonajes().anadirPersonaje(p4);
+		
+		assertEquals(ListaPersonajes.getListaPersonajes().buscarAtributoMasFrecuente(),i1);
+		
+		ListaPersonajes.getListaPersonajes().anadirPersonaje(p5);
+		
+		assertEquals(ListaPersonajes.getListaPersonajes().buscarAtributoMasFrecuente(),i2);
+		
+	}
 
 }
+
